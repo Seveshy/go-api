222392063eb8819a4d8553c41e83de8d2ce5428b
Daniel Major <daniel.major@fiserv.com>
2024-05-27 19:58:08 -0300

    Geranda JWT

--- internal/infra/database/webserver/handlers/user_handlers.go
+++ internal/infra/database/webserver/handlers/user_handlers.go
@@ -4,20 +4,56 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/devfullcycle/goexpert/9-APIS/internal/dto"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/entity"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandlers struct {
-	UserDB database.UserInterface
+	UserDB        database.UserInterface
+	Jwt           *jwtauth.JWTAuth
+	JwtExperiesIn int
 }
 
-func NewUserInterface(userDB database.UserInterface) *UserHandlers {
+func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, JwtExperiesIn int) *UserHandlers {
 	return &UserHandlers{
-		UserDB: userDB,
+		UserDB:        userDB,
+		Jwt:           jwt,
+		JwtExperiesIn: JwtExperiesIn,
 	}
+}
+
+func (h *UserHandlers) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExperiesIn)).Unix(),
+	})
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandlers) CreateUser(w http.ResponseWriter, r *http.Request) {
